100259153b1488a366be7a0ed07152be8d1c11df
zokaphone <Soukrphone5566@gmail.com>
2024-11-19 19:53:19 +0700

    commit
    update

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Login1() {
+  return (
+    <div className='flex flex-row justify-between items-center w-full'>
+      <div className='text-red-200 text- [10rem]'>
+      <h1>Hiii</h1>
+      </div>
+      <div className='text-blue-400 text- [10rem]'>
+        <h1>Helll</h1>
+        </div>
+        <div className='text-blue-400 text- [10rem]'>
+        <h1>Helll</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Login1
